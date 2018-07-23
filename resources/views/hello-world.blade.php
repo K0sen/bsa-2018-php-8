a7417a6850d765c7715e90a17e72534ba244091f
k0sen <caladien12@gmail.com>
2018-07-23 23:51:09 +0300

    docker include

--- resources/views/hello-world.blade.php
+++ resources/views/hello-world.blade.php
@@ -1,10 +1,11 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>Laravel Views Example</title>
-    </head>
-    <body>
-        <h1>Views Homework</h1>
-    </body>
-</html>
+@extends('layouts.layout')
+
+@section('title', 'Laravel Views Example')
+
+@section('header')
+    @parent
+@endsection
+
+@section('content')
+    <h1>Views Homework</h1>
+@endsection
